d99ab498d4dcb6b3d0a2eb4fb671b5c7637d46ba
Maximo Albero <maximo.albero@enti.cat>
2025-12-15 14:11:30 +0100

    Added Move function to Player class

--- source/Player.h
+++ source/Player.h
@@ -23,28 +23,13 @@ public:
 	}
 
 	void Update() override {
-		if (IM->GetEvent(SDLK_S, KeyState::DOWN)) {
-			_physics->AddForce(Vector2(0.f, 20.f));
-		}
-		if (IM->GetEvent(SDLK_W, KeyState::DOWN)) {
-			_physics->AddForce(Vector2(0.f, -20.f));
-		}
-		if (IM->GetEvent(SDLK_A, KeyState::DOWN)) {
-			_physics->AddForce(Vector2(-20.f, 0.f));
-		}
-		if (IM->GetEvent(SDLK_D, KeyState::DOWN)) {
-			_physics->AddForce(Vector2(20.f, 0.f));
-		}
-		if (IM->GetEvent(SDLK_R, KeyState::DOWN)) {
-			_physics->AddTorque(200.f);
-		}
-		if (IM->GetEvent(SDLK_SPACE, KeyState::DOWN)) {
-			Shoot();
-		}
+
+		Move();
 
 		Object::Update();
 	}
 
+	void Move();
 
 	void Shoot() {
 		Bullet* bullet = new Bullet(Vector2(_transform->position.x + 1, _transform->position.y));
