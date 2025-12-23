7bfaa6d6cb7d79083e23c2d7f7078f270e6d7507
Maximo Albero <maximo.albero@enti.cat>
2025-12-23 21:57:19 +0100

    minor change

--- source/Gameplay.h
+++ source/Gameplay.h
@@ -44,12 +44,12 @@ public:
 
 		waves->SetPlayer(player);
 
+		//SPAWNER.SpawnObject(new PowerUp("resources/image.png", Vector2(100.f, 100.f)));
+		//SPAWNER.SpawnObject(new PowerUp1000("resources/image.png", Vector2(128.f, 128.f)));
 
 		TextObject* text = new TextObject("UASINI");
 		text->GetTransform()->position = { 100.0f, 100.0f };
 		_ui.push_back(text);
-		SPAWNER.SpawnObject(new PowerUp("resources/image.png", Vector2(100.f, 100.f)));
-		SPAWNER.SpawnObject(new PowerUp1000("resources/image.png", Vector2(128.f, 128.f)));
 	}
 
 	void OnExit() override { Scene::OnExit(); }
